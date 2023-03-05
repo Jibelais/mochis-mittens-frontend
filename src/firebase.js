055b5bb0cd246ef2930530bc646cd786f1f287a5
Jieun Pivarnik <JPivarnik@jis-macbook-pro.lan>
2023-03-04 22:23:47 -0600

    firebase config created

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC4dKQ_wrSkXcPxU1-CYZhxwf0WRJ8igrs",
+    authDomain: "mochismittens-6990d.firebaseapp.com",
+    projectId: "mochismittens-6990d",
+    storageBucket: "mochismittens-6990d.appspot.com",
+    messagingSenderId: "322658295689",
+    appId: "1:322658295689:web:2cd52e048d857bc7634b58",
+    measurementId: "G-PZ1V96WY3W"
+  };
+
+  const app = initializeApp(firebaseConfig)
